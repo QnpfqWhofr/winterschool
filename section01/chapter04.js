16aa2438f9f0e707aaad72e5583946d114aaddd6
QnpfqWhofr <dsmhwjh0627@dgsw.hs.kr>
2026-01-12 17:04:08 +0900

    e

---
+++ section01/chapter04.js
@@ -0,0 +1,21 @@
+// 1.변수
+let age = 18;
+age = 18;
+
+//2.상수
+const birth = "2009.06.27";
+
+//3.변수명명규칙(네이밍 규칙)
+//3-1. 변수명에 $,_ 제외 사용 불가
+let $_name;
+
+//3-2. 숫자로 시작 불가
+let name1;
+let $1name;
+
+//3-3. 예약어 사용 불가
+
+//4.변수 명명 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
---
+++ section01/chapter05.js
@@ -0,0 +1,28 @@
+//1.number type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let minf = -Infinity;
+
+let nan = NaN;
+
+//2.string type
+let myname = "우종혁";
+let mylocation = '경산';
+let introduce = myname + mylocation;
+
+let introduceText = `${myname}은 ${mylocation}에 거주합니다.`;
+console.log(introduceText);
+
+//3.boolean type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4.null type (아무일도 없었다)
+let empty = null;
+
+//5.undefined type
+let none;
+console.log(none);
