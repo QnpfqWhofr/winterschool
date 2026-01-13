b247850c56f660652e57f310078911e9d6a3a62d
QnpfqWhofr <dsmhwjh0627@dgsw.hs.kr>
2026-01-13 16:03:36 +0900

    r

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,10 @@
-console.log("안녕 node.js");
+import mul, { add, sub } from './math.js';
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
